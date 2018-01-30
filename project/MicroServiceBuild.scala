e55c686a49e82222418880bd0f5e3aae28768eb9
Konrad <wudkowski@gmail.com>
2018-01-30 16:22:51 +0000

    PLATOPS-1201: Cleanup dependencies

--- project/MicroServiceBuild.scala
+++ project/MicroServiceBuild.scala
@@ -1,15 +1,12 @@
-import sbt._
-import play.sbt.PlayImport._
 import play.core.PlayVersion
-import uk.gov.hmrc.SbtAutoBuildPlugin
-import uk.gov.hmrc.sbtdistributables.SbtDistributablesPlugin
-import uk.gov.hmrc.versioning.SbtGitVersioning
+import play.sbt.PlayImport._
+import sbt._
 
 object MicroServiceBuild extends Build with MicroService {
 
   val appName = "slack-notifications"
 
-  override lazy val appDependencies: Seq[ModuleID] = compile ++ test()
+  override lazy val appDependencies: Seq[ModuleID] = compile ++ test
 
   val compile = Seq(
     "uk.gov.hmrc" %% "play-reactivemongo" % "6.2.0",
@@ -17,11 +14,10 @@ object MicroServiceBuild extends Build with MicroService {
     "uk.gov.hmrc" %% "bootstrap-play-25" % "1.3.0"
   )
 
-  def test(scope: String = "test,it") = Seq(
-    "uk.gov.hmrc"       %% "hmrctest"  % "3.0.0"             % scope,
-    "org.scalatest"     %% "scalatest" % "2.2.6"             % scope,
-    "org.pegdown"       % "pegdown"    % "1.6.0"             % scope,
-    "com.typesafe.play" %% "play-test" % PlayVersion.current % scope
-  )
+  val test = Seq(
+    "org.scalatest"     %% "scalatest" % "3.0.4",
+    "org.pegdown"       % "pegdown"    % "1.6.0",
+    "com.typesafe.play" %% "play-test" % PlayVersion.current
+  ).map(_ % "test")
 
 }
