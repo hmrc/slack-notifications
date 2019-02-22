d83465d6aca303d7d215e31d49ba7f5bb3b3de30
Dario Abdulrehman <167463+dabd@users.noreply.github.com>
2019-02-22 09:30:34 +0000

    PLATOPS-1850: Library security upgrades

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -5,18 +5,18 @@ import sbt._
 object AppDependencies {
 
   val compile = Seq(
-    "uk.gov.hmrc"           %% "play-reactivemongo" % "6.2.0",
+    "uk.gov.hmrc"           %% "play-reactivemongo" % "6.4.0",
     "org.typelevel"         %% "cats-core"          % "1.0.1",
-    "uk.gov.hmrc"           %% "bootstrap-play-25"  % "1.7.0",
+    "uk.gov.hmrc"           %% "bootstrap-play-25"  % "4.9.0",
     "com.github.pureconfig" %% "pureconfig"         % "0.9.1",
     ws,
     cache
   )
 
   val test = Seq(
-    "org.scalatest"          %% "scalatest"          % "3.0.4"             % "test",
+    "org.scalatest"          %% "scalatest"          % "3.0.5"             % "test",
     "org.scalacheck"         %% "scalacheck"         % "1.13.4"            % "test",
-    "org.scalatestplus.play" %% "scalatestplus-play" % "2.0.0"             % "test",
+    "org.scalatestplus.play" %% "scalatestplus-play" % "2.0.1"             % "test",
     "org.pegdown"            % "pegdown"             % "1.6.0"             % "test",
     "org.mockito"            % "mockito-all"         % "1.10.19"           % "test",
     "com.github.tomakehurst" % "wiremock"            % "1.52"              % "test",
