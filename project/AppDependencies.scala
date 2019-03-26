6ed8b0985ad16e05dafe1301f381d76db240fa14
Tim Bennett <2475478+bennetimo@users.noreply.github.com>
2019-03-26 15:18:38 +0000

    remove references to mongo

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -5,21 +5,20 @@ import sbt._
 object AppDependencies {
 
   val compile = Seq(
-    "uk.gov.hmrc"           %% "play-reactivemongo" % "6.4.0",
-    "org.typelevel"         %% "cats-core"          % "1.0.1",
+    "org.typelevel"         %% "cats-core"          % "1.6.0",
     "uk.gov.hmrc"           %% "bootstrap-play-25"  % "4.9.0",
-    "com.github.pureconfig" %% "pureconfig"         % "0.9.1",
+    "com.github.pureconfig" %% "pureconfig"         % "0.9.2",
     ws,
     cache
   )
 
   val test = Seq(
-    "org.scalatest"          %% "scalatest"          % "3.0.5"             % "test",
+    "org.scalatest"          %% "scalatest"          % "3.0.7"             % "test",
     "org.scalacheck"         %% "scalacheck"         % "1.13.4"            % "test",
     "org.scalatestplus.play" %% "scalatestplus-play" % "2.0.1"             % "test",
     "org.pegdown"            % "pegdown"             % "1.6.0"             % "test",
     "org.mockito"            % "mockito-all"         % "1.10.19"           % "test",
-    "com.github.tomakehurst" % "wiremock"            % "1.52"              % "test",
+    "com.github.tomakehurst" % "wiremock"            % "2.22.0"              % "test",
     "com.typesafe.play"      %% "play-test"          % PlayVersion.current % "test"
   )
 
